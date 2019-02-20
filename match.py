94f0980da6259b609eac76d72763314e3f05dc19
Eugen17 <eugen17ua@ukr.net>
2019-02-20 08:28:02 +0300

    Update match.py

--- match.py
+++ match.py
@@ -1,8 +1,8 @@
 from mongoengine import *
 
 
-connect('matches',host='ds223685.mlab.com', port=23685,username='heroku_xs74xv52', password='6ak8s2geflvkb2tl3rudf5bikn')
-
+#connect('matches',host='ds223685.mlab.com', port=23685,username='heroku_xs74xv52', password='6ak8s2geflvkb2tl3rudf5bikn')
+connect('matches',host='mongodb://heroku_xs74xv52:6ak8s2geflvkb2tl3rudf5bikn@ds223685.mlab.com:23685/heroku_xs74xv52')
 
 class Match(Document):
     ID = IntField()
